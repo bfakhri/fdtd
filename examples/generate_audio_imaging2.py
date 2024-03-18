241121c65eb5bceb3d0b074339d49f8d64e01cf8
Bijan Fakhri <bfakhri@asu.edu>
2024-03-18 01:32:16 -0500

    U

--- examples/generate_audio_imaging2.py
+++ examples/generate_audio_imaging2.py
@@ -24,33 +24,44 @@ def NormalizeEnergy(energy, width=3):
     return energy_normed
 
 #for shift in range(0, 143, 1):
-for shift in range(0, 2*57, 1):
-    grid = fdtd.Grid(
-        (3.5e-5, 3.5e-5, 1),
-        #(1.5e-5, 1.5e-5, 1),
-        grid_spacing=0.1 * WAVELENGTH,
-        permittivity=1.0,
-        permeability=1.0,
-    )
-    grid[0:10, :, :] = fdtd.PML(name="pml_xlow")
-    grid[-10:, :, :] = fdtd.PML(name="pml_xhigh")
-    grid[:, 0:10, :] = fdtd.PML(name="pml_ylow")
-    grid[:, -10:, :] = fdtd.PML(name="pml_yhigh")
-    grid[:, :, 0] = fdtd.PeriodicBoundary(name="zbounds")
+period_steps = 114
+major_step = 0
+directions = [(-1.0, 0.0), (1.0, 0.0), (0.0, -1.0), (0.0, 1.0)]
+l_delay = period_steps//2
+r_delay = period_steps//2
 
-    y_mid, x_mid = grid.shape[0]//2, grid.shape[1]//2
+for l_dir, r_dir in directions:
+    for shift in range(0, period_steps//2, 1):
+        grid = fdtd.Grid(
+            (3.5e-5, 3.5e-5, 1),
+            #(1.5e-5, 1.5e-5, 1),
+            grid_spacing=0.1 * WAVELENGTH,
+            permittivity=1.0,
+            permeability=1.0,
+        )
+        grid[0:10, :, :] = fdtd.PML(name="pml_xlow")
+        grid[-10:, :, :] = fdtd.PML(name="pml_xhigh")
+        grid[:, 0:10, :] = fdtd.PML(name="pml_ylow")
+        grid[:, -10:, :] = fdtd.PML(name="pml_yhigh")
+        grid[:, :, 0] = fdtd.PeriodicBoundary(name="zbounds")
 
-    grid[y_mid, 25, 0] = fdtd.PointSource(
-        period=WAVELENGTH2 / SPEED_LIGHT, name="linesource0", amplitude=1,
-    )
-    grid[y_mid, -25, 0] = fdtd.PointSource(
-        period=WAVELENGTH2 / SPEED_LIGHT, name="linesource1", delay=shift,
-    )
-    
-    print('Running with shift {0} and source period {1}'.format(shift, grid.sources[0].period))
-    grid.run(1000, progress_bar=False)
-    E_energy_avg = bd.numpy(bd.sum(grid.E_avg[:, :, 0, :]**2, axis=-1))
-    E_energy_avg_normed = NormalizeEnergy(E_energy_avg)
-    plt.axis('off')
-    plt.imshow(E_energy_avg_normed)
-    plt.savefig('ears/avg_energy_shift={0}.png'.format(shift), bbox_inches='tight')
+        y_mid, x_mid = grid.shape[0]//2, grid.shape[1]//2
+
+        grid[y_mid, 25, 0] = fdtd.PointSource(
+            period=WAVELENGTH2 / SPEED_LIGHT, name="left_source", delay=l_delay,
+        )
+        grid[y_mid, -25, 0] = fdtd.PointSource(
+            period=WAVELENGTH2 / SPEED_LIGHT, name="right_source", delay=r_delay,
+        )
+
+        l_delay += l_dir
+        r_delay += r_dir
+        
+        print('Step: {0}\tRunning with delays ({1}, {2}) and source period {3}'.format(major_step, l_delay, r_delay, grid.sources[0].period))
+        grid.run(1000, progress_bar=False)
+        E_energy_avg = bd.numpy(bd.sum(grid.E_avg[:, :, 0, :]**2, axis=-1))
+        E_energy_avg_normed = NormalizeEnergy(E_energy_avg)
+        plt.axis('off')
+        plt.imshow(E_energy_avg_normed)
+        plt.savefig('ears/avg_energy_{0:04d}.png'.format(major_step), bbox_inches='tight')
+        major_step += 1
